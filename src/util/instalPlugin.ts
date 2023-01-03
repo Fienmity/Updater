cffa6ea77820a41333cd4fe0177ef6826e241284
Jack Matthews <jm5112356@gmail.com>
2023-01-03 02:02:38 -0500

    delete old functions

--- src/util/instalPlugin.ts
+++
@@ -1,3 +0,0 @@
-export default function installPlugin(url: string, callback?: (result) => void, update?: () => void): Promise<void> {
-	return window.enmity.plugins.installPlugin(url, callback, update);
-}
